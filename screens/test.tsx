05236202c8afe65a15dddf49657d5da67f035e41
vu.nguyenvan <vunv@falcongames.com>
2023-10-10 10:07:15 +0700

    test react-native-pdf

--- screens/test.tsx
+++ screens/test.tsx
@@ -1,55 +1,35 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {CastButton, useRemoteMediaClient} from 'react-native-google-cast';
-import Video from 'react-native-video';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import Pdf from 'react-native-pdf';
 
 export function CastComponent() {
-  // This will automatically rerender when client is connected to a device
-  // (after pressing the button that's rendered below)
-  const client = useRemoteMediaClient();
-
-  useEffect(() => {
-    console.log('client :>> ', client);
-  }, [client]);
-
-  if (client) {
-    // Send the media to your Cast device as soon as we connect to a device
-    // (though you'll probably want to call this later once user clicks on a video or something)
-    client.loadMedia({
-      mediaInfo: {
-        contentUrl:
-          'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4',
-        contentType: 'video/mp4',
-      },
-    });
-  }
+  const source = {
+    // uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+    uri: 'https://pspdfkit.com/downloads/pspdfkit-android-quickstart-guide.pdf',
+    cache: true,
+  };
 
   // This will render native Cast button.
   // When a user presses it, a Cast dialog will prompt them to select a Cast device to connect to.
   return (
-    <View>
-      <CastButton
-        style={{
-          width: 24,
-          height: 24,
-          tintColor: 'black',
-          marginTop: 10,
-          paddingHorizontal: 36,
+    <View style={styles.container}>
+      {/* <View>AAA</View> */}
+      <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
         }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log(`Current page: ${page}`);
+        }}
+        onError={error => {
+          console.log(error);
+        }}
+        onPressLink={uri => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+        style={styles.pdf}
       />
-      <View style={styles.container}>
-        <Video
-          source={{
-            uri: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-          }} // Thay đổi URL của video tại đây
-          controls={true}
-          resizeMode="cover"
-          // resizeMode="contain" // Chế độ tỷ lệ khung hình
-          // hideShutterView={true}
-          style={styles.video}
-          // onEnd={() => {}}
-        />
-      </View>
     </View>
   );
 }
@@ -57,11 +37,16 @@ export function CastComponent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    marginTop: 25,
   },
-  video: {
+  pdf: {
+    flex: 1,
     width: 300,
-    height: 200,
+    height: 350,
+    backgroundColor: '#e1e1e1',
+    // width: Dimensions.get('window').width,
+    // height: Dimensions.get('window').height,
   },
 });
