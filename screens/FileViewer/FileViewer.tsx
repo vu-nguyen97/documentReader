d82149cfee136c2ffd31b8333f9b2d49488ba064
vu.nguyenvan <vunv@falcongames.com>
2023-10-27 11:46:41 +0700

    Handle unzip file

--- screens/FileViewer/FileViewer.tsx
+++ screens/FileViewer/FileViewer.tsx
@@ -5,6 +5,7 @@ import PdfViewer from './Pdf/PdfViewer';
 import TextViewer from './Text/TextViewer';
 import WordViewer from './Word/WordViewer';
 import ExcelViewer from './Excel/ExcelViewer';
+import ZipViewer from './Zip/ZipViewer';
 import FileNotSupport from './FileNotSupport';
 
 export default function FileViewer({navigation}: any) {
@@ -18,8 +19,9 @@ export default function FileViewer({navigation}: any) {
     setFile(undefined);
   };
 
-  const viewerProps = {file, handleBack};
+  const viewerProps = {file, handleBack, setFile};
   let ViewerComp;
+  // console.log('file?.type :>> ', file?.type);
   switch (file?.type) {
     case 'application/pdf':
       ViewerComp = <PdfViewer {...viewerProps} />;
@@ -28,12 +30,17 @@ export default function FileViewer({navigation}: any) {
       ViewerComp = <TextViewer {...viewerProps} />;
       break;
     case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+      // case 'application/msword':
       ViewerComp = <WordViewer {...viewerProps} />;
       break;
     case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
       ViewerComp = <ExcelViewer {...viewerProps} />;
       break;
+    case 'application/zip':
+      ViewerComp = <ZipViewer {...viewerProps} />;
+      break;
 
+    // case 'application/rar':
     default:
       ViewerComp = <FileNotSupport {...viewerProps} />;
       break;
