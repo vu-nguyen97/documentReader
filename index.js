19cc0cbbdceefe320eb541bcf681d5db93ceacf3
TungK64 <tungvp16@gmail.com>
2023-10-30 13:33:20 +0700

    hot-fix/RNDPRD2DOC: fix function word viewer

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import TextEncodingPolyfill from 'text-encoding';
+
+Object.assign(global, {
+  TextEncoder: TextEncodingPolyfill.TextEncoder,
+  TextDecoder: TextEncodingPolyfill.TextDecoder,
+});
 
 AppRegistry.registerComponent(appName, () => App);
