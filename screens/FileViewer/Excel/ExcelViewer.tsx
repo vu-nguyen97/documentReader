66a9088eddd741ccb67044c3c7ac44557cebecdd
TungK64 <tungvp16@gmail.com>
2023-11-01 14:41:12 +0700

    hotfix/RNDPRD2DOC-22: fix function view excel file

--- screens/FileViewer/Excel/ExcelViewer.tsx
+++ screens/FileViewer/Excel/ExcelViewer.tsx
@@ -1,5 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {Dimensions, StyleSheet, View, Text, ScrollView} from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
 import {Appbar} from 'react-native-paper';
 import XLSX from 'xlsx';
 import {DataTable} from 'react-native-paper';
@@ -7,51 +15,51 @@ import RNFS from 'react-native-fs';
 import {MORE_ICON} from '../../../components/common/Helpers/UIHelpers';
 import Loading from '../../../components/common/Loading/Loading';
 import {LOAD_FILE} from '../../../components/constants/constants';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 function getHeaderRow(workSheet: any) {
   const headers = [];
   const range = XLSX.utils.decode_range(workSheet['!ref'] ?? '');
-  const row = range.s.r;
-  for (let col = range.s.c; col <= range.e.c; ++col) {
-    const cell =
-      workSheet[
-        XLSX.utils.encode_cell({c: col, r: row})
-      ]; /* find the cell in the first row */
+  const row = 0;
+  for (let col = 0; col <= range.e.c; ++col) {
+    const cell = workSheet[XLSX.utils.encode_cell({c: col, r: row})];
     const header = cell && cell.t ? XLSX.utils.format_cell(cell) : 'unknown';
-    if (header != 'unknown') headers.push(header);
+    if (header != 'unknown') headers[col] = header;
+    else headers[col] = null;
   }
   return headers;
 }
 
 function readExcelData(workSheet: any, headers: any) {
-  const data = []; // Mảng lưu trữ dữ liệu từ từng dòng
-  let length = 0;
-
+  const data = [];
   const range = XLSX.utils.decode_range(workSheet['!ref'] ?? '');
-  for (let row = range.s.r; row <= range.e.r; row++) {
-    const cell = workSheet['A' + row];
-    if (cell && cell.v !== null && cell.v !== undefined) {
-      length++;
+  let maxRow = 0;
+
+  for (let row = 0; row <= range.e.r; row++) {
+    for (let col = 0; col <= range.e.c; col++) {
+      const cell = workSheet[XLSX.utils.encode_cell({c: col, r: row})];
+      if (cell && cell.t) {
+        maxRow = Math.max(maxRow, row);
+      }
     }
   }
 
-  for (let row = range.s.r + 1; row <= length - 1; ++row) {
-    let rowData = {};
+  for (let row = 0; row <= maxRow; row++) {
+    const rowData = {};
 
-    for (let col = range.s.c; col < headers.length; ++col) {
+    for (let col = 0; col <= range.e.c; col++) {
       const cell = workSheet[XLSX.utils.encode_cell({c: col, r: row})];
 
-      // Lấy tên cột từ mảng tiêu đề (đã được trích xuất ở trước)
-      const columnName = headers[col];
-
-      // Lấy giá trị từ ô và thêm vào đối tượng dữ liệu
-      rowData[columnName] = cell ? XLSX.utils.format_cell(cell) : null;
+      if (cell && cell.v !== null && cell.v !== undefined) {
+        rowData[col] = XLSX.utils.format_cell(cell);
+      } else {
+        rowData[col] = null;
+      }
     }
 
-    // Thêm dữ liệu của dòng hiện tại vào mảng data
     data.push(rowData);
   }
-
+  data.shift();
   return data;
 }
 
@@ -62,17 +70,20 @@ export default function ExcelViewer(props: any) {
   const [tableData, setTableData] = useState<any[]>([]);
   const [tableHeaders, setTableHeaders] = useState<String[]>([]);
   const [columnWidths, setColumnWidths] = useState({});
+  const [sheetNumber, setSheetNumber] = useState<any>(0);
+  const [sheetNames, setSheetNames] = useState<any>([]);
 
   useEffect(() => {
     if (!file) return;
     viewExcelFile();
-  }, [file]);
+  }, [file, sheetNumber]);
 
   const viewExcelFile = async () => {
     setIsLoading(true);
     const data = await RNFS.readFile(file.fileCopyUri, 'base64');
     const workBook = XLSX.read(data, {type: 'base64'});
-    const firstSheet = workBook.SheetNames[0];
+    setSheetNames(workBook.SheetNames);
+    const firstSheet = workBook.SheetNames[sheetNumber];
     const workSheet = workBook.Sheets[firstSheet];
 
     setIsLoading(false);
@@ -85,14 +96,13 @@ export default function ExcelViewer(props: any) {
   };
 
   useEffect(() => {
-    // Calculate the maximum width for each column
     const widths = {};
-    tableHeaders.forEach(header => {
+    tableHeaders.forEach((header, index) => {
       const maxWidth = Math.max(
-        ...tableData.map(rowData => String(rowData[header]).length),
+        ...tableData.map(rowData => String(rowData[index]).length),
         String(header).length,
       );
-      widths[header] = maxWidth * 8; // Adjust the multiplier to fit your content
+      widths[index] = maxWidth * 15;
     });
     setColumnWidths(widths);
   }, [tableData]);
@@ -102,56 +112,103 @@ export default function ExcelViewer(props: any) {
   if (isLoading) return <Loading text={LOAD_FILE} />;
 
   return (
-    <ScrollView>
-      <Appbar.Header style={styles.headerMenu}>
-        <Appbar.BackAction onPress={handleBack} />
-        <Appbar.Content title="Title" />
-        <Appbar.Action icon="magnify" onPress={() => {}} />
-        <Appbar.Action icon={MORE_ICON} onPress={openMoreAction} />
-      </Appbar.Header>
-      {tableData.length > 0 && (
-        <ScrollView horizontal={true}>
-          <View style={styles.table}>
-            <DataTable>
-              <DataTable.Header>
-                {tableHeaders.map((header, index) => (
-                  <DataTable.Title
-                    key={index}
-                    style={[
-                      styles.tableHeaders,
-                      {
-                        borderLeftWidth: index === 0 ? 1 : 0,
-                        width: columnWidths[header],
-                      },
-                    ]}>
-                    {header}
-                  </DataTable.Title>
-                ))}
-              </DataTable.Header>
-
-              {tableData.map((rowData, rowIndex) => (
-                <DataTable.Row key={rowIndex}>
-                  {tableHeaders.map((header, headerIndex) => (
-                    <DataTable.Cell
-                      key={headerIndex}
+    <SafeAreaView>
+      <ScrollView>
+        <Appbar.Header style={styles.headerMenu}>
+          <Appbar.BackAction onPress={handleBack} />
+          <Appbar.Content title="Title" />
+          <Appbar.Action icon="magnify" onPress={() => {}} />
+          <Appbar.Action icon={MORE_ICON} onPress={openMoreAction} />
+        </Appbar.Header>
+        {(tableData.length > 0 || Headers.length > 0) && (
+          <ScrollView horizontal={true} style={{marginBottom: 40}}>
+            <View style={styles.table}>
+              <DataTable>
+                <DataTable.Header>
+                  {tableHeaders.map((header, index) => (
+                    <DataTable.Title
+                      key={index}
                       style={[
-                        styles.dataCell,
+                        styles.tableHeaders,
                         {
-                          borderLeftWidth: headerIndex === 0 ? 1 : 0,
-                          flex: 1,
-                          width: columnWidths[header],
+                          borderLeftWidth: index === 0 ? 1 : 0,
+                          width: columnWidths[index],
                         },
                       ]}>
-                      <Text style={styles.cellText}>{rowData[header]}</Text>
-                    </DataTable.Cell>
+                      {header}
+                    </DataTable.Title>
                   ))}
-                </DataTable.Row>
-              ))}
-            </DataTable>
-          </View>
-        </ScrollView>
-      )}
-    </ScrollView>
+                </DataTable.Header>
+
+                {tableData.map((rowData, rowIndex) => (
+                  <DataTable.Row key={rowIndex}>
+                    {tableHeaders.map((header, headerIndex) => (
+                      <DataTable.Cell
+                        key={headerIndex}
+                        style={[
+                          styles.dataCell,
+                          {
+                            borderLeftWidth: headerIndex === 0 ? 1 : 0,
+                            flex: 1,
+                            width: columnWidths[headerIndex],
+                          },
+                        ]}>
+                        <Text style={styles.cellText}>
+                          {rowData[headerIndex]}
+                        </Text>
+                      </DataTable.Cell>
+                    ))}
+                  </DataTable.Row>
+                ))}
+              </DataTable>
+            </View>
+          </ScrollView>
+        )}
+      </ScrollView>
+
+      <ScrollView
+        horizontal={true}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: '#FFFFFF',
+        }}>
+        <View
+          style={{
+            paddingLeft: 15,
+            paddingBottom: 10,
+            paddingTop: 5,
+            paddingRight: 15,
+          }}>
+          {sheetNames.length > 1 && (
+            <View style={styles.sheetButtonRow}>
+              {sheetNames.map((sheetName: any, index: any) =>
+                index == Number(sheetNumber) ? (
+                  <TouchableOpacity
+                    style={[
+                      styles.sheetButton,
+                      {borderColor: 'blue', backgroundColor: 'lightblue'},
+                    ]}
+                    key={index}
+                    onPress={() => (setSheetNumber(index), viewExcelFile)}>
+                    <Text style={{fontWeight: 'bold'}}>{sheetName}</Text>
+                  </TouchableOpacity>
+                ) : (
+                  <TouchableOpacity
+                    style={styles.sheetButton}
+                    key={index}
+                    onPress={() => (setSheetNumber(index), viewExcelFile)}>
+                    <Text style={{fontWeight: 'bold'}}>{sheetName}</Text>
+                  </TouchableOpacity>
+                ),
+              )}
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -171,7 +228,7 @@ const styles = StyleSheet.create({
   },
   tableHeaders: {
     backgroundColor: 'lightblue',
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
     paddingHorizontal: 8,
     borderRightWidth: 1,
@@ -188,5 +245,19 @@ const styles = StyleSheet.create({
   },
   cellText: {
     flexWrap: 'wrap', // Enable text wrapping in cells
+  },
+  sheetButtonRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+    alignContent: 'space-around',
+  },
+  sheetButton: {
+    marginRight: 10,
+    padding: 4,
+    marginBottom: 5,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
